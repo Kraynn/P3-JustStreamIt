b66c389c22bf996ae27c1ea6f01769fdf3be5128
Kraynn <75184473+Kraynn@users.noreply.github.com>
2022-09-28 05:59:21 +0100

    Structure de base HTML de l'interface

---
+++ script.js
@@ -0,0 +1,110 @@
+let slider = tns({
+    container : ".my-slider",
+    "speed" : 500,
+    "nav" : false,
+    autoplay : true,
+    controls: false,
+    autoplayButtonOutput : false,
+    responsive: {
+        1800: {
+            items: 6,
+            gutter:50,
+        },
+        1600 : {
+            items: 5,
+            gutter: 50,
+        },
+        1024: {
+            items: 3,
+            gutter: 30
+        },
+        768: {
+            items: 2,
+            gutter: 10
+        }
+    }
+})
+
+let slider_cat1 = tns({
+    container : ".my-slider-cat1",
+    "slideBy" : "2",
+    "speed" : 500,
+    "nav" : false,
+    autoplay : true,
+    controls: false,
+    autoplayButtonOutput : false,
+    responsive: {
+        1800: {
+            items: 6,
+            gutter:50,
+        },
+        1600 : {
+            items: 5,
+            gutter: 50,
+        },
+        1024: {
+            items: 3,
+            gutter: 30
+        },
+        768: {
+            items: 2,
+            gutter: 10
+        }
+    }
+})
+
+let slider_cat2 = tns({
+    container : ".my-slider-cat2",
+    "slideBy" : "2",
+    "speed" : 500,
+    "nav" : false,
+    autoplay : true,
+    controls: false,
+    autoplayButtonOutput : false,
+    responsive: {
+        1800: {
+            items: 6,
+            gutter:50,
+        },
+        1600 : {
+            items: 5,
+            gutter: 50,
+        },
+        1024: {
+            items: 3,
+            gutter: 30
+        },
+        768: {
+            items: 2,
+            gutter: 10
+        }
+    }
+})
+
+let slider_cat3 = tns({
+    container : ".my-slider-cat3",
+    "slideBy" : "2",
+    "speed" : 500,
+    "nav" : false,
+    autoplay : true,
+    controls: false,
+    autoplayButtonOutput : false,
+    responsive: {
+        1800: {
+            items: 6,
+            gutter:50,
+        },
+        1600 : {
+            items: 5,
+            gutter: 50,
+        },
+        1024: {
+            items: 3,
+            gutter: 30
+        },
+        768: {
+            items: 2,
+            gutter: 10
+        }
+    }
+})
